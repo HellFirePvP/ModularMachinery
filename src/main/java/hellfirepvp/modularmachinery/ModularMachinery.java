6cd8fe63e80195c4fe420919005a4a36c11f0af5
HellFirePvP <dendorfer.maximilian0@googlemail.com>
2017-08-24 11:32:30 +0200

    Version bump to 1.5

--- src/main/java/hellfirepvp/modularmachinery/ModularMachinery.java
+++ src/main/java/hellfirepvp/modularmachinery/ModularMachinery.java
@@ -43,7 +43,7 @@ public class ModularMachinery {
 
     public static final String MODID = "modularmachinery";
     public static final String NAME = "Modular Machinery";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.5";
     public static final String CLIENT_PROXY = "hellfirepvp.modularmachinery.client.ClientProxy";
     public static final String COMMON_PROXY = "hellfirepvp.modularmachinery.common.CommonProxy";
 
