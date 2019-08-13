409708cca8f5a2a1d966d14ed1fc871da86fedd2
HellFirePvP <7419378+HellFirePvP@users.noreply.github.com>
2019-08-13 21:50:49 +0200

    Actually bump version number

--- src/main/java/hellfirepvp/modularmachinery/ModularMachinery.java
+++ src/main/java/hellfirepvp/modularmachinery/ModularMachinery.java
@@ -43,7 +43,7 @@ public class ModularMachinery {
 
     public static final String MODID = "modularmachinery";
     public static final String NAME = "Modular Machinery";
-    public static final String VERSION = "1.11.0";
+    public static final String VERSION = "1.11.1";
     public static final String CLIENT_PROXY = "hellfirepvp.modularmachinery.client.ClientProxy";
     public static final String COMMON_PROXY = "hellfirepvp.modularmachinery.common.CommonProxy";
 
